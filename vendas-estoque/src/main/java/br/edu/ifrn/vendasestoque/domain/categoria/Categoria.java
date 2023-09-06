0e11e4f79be909fe852d925e8c265c87767e1b3b
Kelvin <kelvinmarquesk@gmail.com>
2023-09-06 09:57:03 -0300

    adicionada as propriedades para conexão com o database e criado domain para categoria

---
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/categoria/Categoria.java
@@ -0,0 +1,25 @@
+package br.edu.ifrn.vendasestoque.domain.categoria;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="categoria")
+@Entity(name = "categoria")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+}
