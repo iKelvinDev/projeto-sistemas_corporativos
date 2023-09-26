0b48bf4e06460999c4090ff12c7854870dc2e595
Kelvin <kelvinmarquesk@gmail.com>
2023-09-26 19:58:59 -0300

    add domain (Produto)

---
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/produto/Produto.java
@@ -0,0 +1,67 @@
+package br.edu.ifrn.vendasestoque.domain.produto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    private String descricao;
+
+    private int quantidadeEstoque;
+
+    private BigDecimal preco;
+
+    //Getters
+    //Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
