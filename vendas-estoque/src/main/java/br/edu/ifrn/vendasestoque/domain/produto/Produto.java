48405bc8bc1b82f07e7eee34d210e0e84a998d60
Kelvin <kelvinmarquesk@gmail.com>
2023-09-26 20:11:03 -0300

    alterado a implementação das entidades de domain Produto

--- vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/produto/Produto.java
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/produto/Produto.java
@@ -6,8 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Produto {
 
     @Id
@@ -22,46 +28,4 @@ public class Produto {
     private int quantidadeEstoque;
 
     private BigDecimal preco;
-
-    //Getters
-    //Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public int getQuantidadeEstoque() {
-        return quantidadeEstoque;
-    }
-
-    public void setQuantidadeEstoque(int quantidadeEstoque) {
-        this.quantidadeEstoque = quantidadeEstoque;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
 }
