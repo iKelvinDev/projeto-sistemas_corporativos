5d5e4891c0a9b4712b8d3a697b1466ac40dd6c15
Kelvin <kelvinmarquesk@gmail.com>
2023-09-26 22:27:29 -0300

    Implementação de FabricanteController

---
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/controller/FabricanteController.java
@@ -0,0 +1,77 @@
+package br.edu.ifrn.vendasestoque.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.edu.ifrn.vendasestoque.domain.fabricante.Fabricante;
+import br.edu.ifrn.vendasestoque.repository.FabricanteRepository;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("fabricantes")
+public class FabricanteController {
+    @Autowired
+    private FabricanteRepository repository;
+
+    @PostMapping
+    @Transactional
+
+    public ResponseEntity cadastrar(@RequestBody @Valid Fabricante fabricante, 
+                                            UriComponentsBuilder uriBuilder){
+        Fabricante fabricanteLocal = repository.save(fabricante);
+        var uri = uriBuilder.path("/fabricante/{id}").buildAndExpand(fabricanteLocal.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var fabricante = repository.findById(id);
+        return ResponseEntity.ok(fabricante);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Fabricante>> listar(@PageableDefault(size=4, 
+                                        sort = {"nome"}) Pageable paginacao){
+        var fabricante = repository.findAll(paginacao);
+        return ResponseEntity.ok(fabricante);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id){
+        var fabricante = repository.getReferenceById(id);
+        repository.delete(fabricante);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Fabricante> atualizar(@PathVariable Long id, @RequestBody @Valid Fabricante fabricante){
+        Optional<Fabricante> fabricanteOptional = repository.findById(id);
+    
+        if (fabricanteOptional.isPresent()) {
+            Fabricante fabricanteLocal = fabricanteOptional.get();
+            fabricanteLocal.setNome(fabricante.getNome());
+            repository.save(fabricanteLocal);
+            return ResponseEntity.ok(fabricanteLocal);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
