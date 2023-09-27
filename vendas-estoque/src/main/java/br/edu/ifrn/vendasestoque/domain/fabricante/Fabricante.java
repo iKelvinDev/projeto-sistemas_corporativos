23657f1b54a9bb6afbf05c03b4745791f9340fb9
Kelvin <kelvinmarquesk@gmail.com>
2023-09-26 21:53:04 -0300

    add domain (Fabricante)

---
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/fabricante/Fabricante.java
@@ -0,0 +1,30 @@
+package br.edu.ifrn.vendasestoque.domain.fabricante;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
+public class Fabricante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+}
