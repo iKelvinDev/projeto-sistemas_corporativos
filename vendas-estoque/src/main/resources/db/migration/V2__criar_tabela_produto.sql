91a460f44f2676ea95f80f716c56ce07ecbdcf68
kelvin <kelvinmarquesk@gmail.com>
2023-09-27 09:36:40 -0300

    Alteração de migration da tabela produto (add foreign key)

--- vendas-estoque/src/main/resources/db/migration/V2__criar_tabela_produto.sql
+++ vendas-estoque/src/main/resources/db/migration/V2__criar_tabela_produto.sql
@@ -2,6 +2,10 @@ create table produto (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(250) NOT NULL,
-    quantidadeEstoque INT NOT NULL,
-    preco DECIMAL NOT NULL
+    quantidade_estoque INT NOT NULL,
+    preco DECIMAL NOT NULL,
+    fabricante_id INT NOT NULL,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id),
+    FOREIGN KEY (fabricante_id) REFERENCES fabricante(id)
 );
