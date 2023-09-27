37981a8a650a5790162ac0aaafa489cc3c11618a
kelvin <kelvinmarquesk@gmail.com>
2023-09-27 07:30:09 -0300

    Correção de métodos de CategoriaController

--- vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
@@ -1,15 +1,14 @@
 package br.edu.ifrn.vendasestoque.controller;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,26 +17,28 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.edu.ifrn.vendasestoque.domain.categoria.Categoria;
 import br.edu.ifrn.vendasestoque.repository.CategoriaRepository;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
 @RequestMapping("categorias")
 public class CategoriaController {
-    
+
     @Autowired
     private CategoriaRepository repository;
 
     @PostMapping
     @Transactional
-    public ResponseEntity cadastrar(@RequestBody Categoria categoria, UriComponentsBuilder uriBuilder){
-        Categoria categoriaLocal = repository.save(categoria); 
-        var uri = uriBuilder.path("/categorias/{id}").buildAndExpand(categoriaLocal.getId()).toUri();
+    public ResponseEntity cadastrar(@RequestBody @Valid Categoria categoria, 
+                                            UriComponentsBuilder uriBuilder){
+        Categoria categoriaLocal = repository.save(categoria);
+        var uri = uriBuilder.path("/categorias/{id}").
+                  buildAndExpand(categoriaLocal.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
@@ -48,7 +49,8 @@ public class CategoriaController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<Categoria>> listar(@PageableDefault(size=4, sort = {"nome"}) Pageable paginacao){
+    public ResponseEntity<Page<Categoria>> listar(@PageableDefault(size=4, 
+                                        sort = {"nome"}) Pageable paginacao){
         var categorias = repository.findAll(paginacao);
         return ResponseEntity.ok(categorias);
     }
@@ -57,16 +59,18 @@ public class CategoriaController {
     @Transactional
     public ResponseEntity excluir(@PathVariable Long id){
         var categoria = repository.getReferenceById(id);
-        log.info(categoria);
         repository.delete(categoria);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping
-    public ResponseEntity atualizar(@RequestBody Categoria categoria){
-        var categoriaLocal = repository.getReferenceById(categoria.getId());
-        repository.save(categoria);
+    @Transactional
+    public ResponseEntity<Categoria> atualizar(@RequestBody @Valid Categoria categoria){
+        Categoria categoriaLocal = repository.findById(categoria.getId()).get();
+ 
+        categoriaLocal.setNome(categoria.getNome());
+
         return ResponseEntity.ok(categoriaLocal);
     }
-
+    
 }
