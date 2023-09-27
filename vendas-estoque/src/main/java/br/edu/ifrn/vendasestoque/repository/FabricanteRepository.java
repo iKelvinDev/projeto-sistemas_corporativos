6768239ad4198f69ad44ecf4b320b5ce7d16c235
Kelvin <kelvinmarquesk@gmail.com>
2023-09-26 21:55:49 -0300

    add FabricanteRepository

---
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/repository/FabricanteRepository.java
@@ -0,0 +1,7 @@
+package br.edu.ifrn.vendasestoque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.edu.ifrn.vendasestoque.domain.fabricante.Fabricante;
+
+public interface FabricanteRepository extends JpaRepository<Fabricante, Long> {
+}
