5b8198c3e10343fda893454c51bbfcab506822e2
Kelvin <kelvinmarquesk@gmail.com>
2023-10-09 17:46:37 -0300

    produto repository

--- vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/repository/ProdutoRepository.java
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/repository/ProdutoRepository.java
@@ -1,9 +1,14 @@
 package br.edu.ifrn.vendasestoque.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifrn.vendasestoque.domain.produto.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
+    Page<Produto> findAllByAtivoTrue(Pageable pageable);
+    Produto findByNome(String nome);
 }
