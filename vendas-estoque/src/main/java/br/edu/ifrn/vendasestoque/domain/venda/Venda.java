4cc9fec3abca4afef6fc86decbc43c041b244104
kelvin <kelvinmarquesk@gmail.com>
2023-10-18 08:06:51 -0300

    Add strategy for fetching data (EAGER) in Domain Venda

--- vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/venda/Venda.java
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/venda/Venda.java
@@ -8,6 +8,7 @@ import br.edu.ifrn.vendasestoque.domain.cliente.Cliente;
 import br.edu.ifrn.vendasestoque.domain.itemvenda.ItemVenda;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +38,7 @@ public class Venda {
     private LocalDateTime dataHora;
     @Column(name = "total_venda")
     private BigDecimal totalVenda;
-    @OneToMany(mappedBy = "venda")
+    @OneToMany(mappedBy = "venda", fetch = FetchType.EAGER)
     private List<ItemVenda> itensVendas;
     @ManyToOne
     @JoinColumn(name = "cliente_id")
