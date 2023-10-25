de7620e3ada479eb0a325dc0d7d5574dd2ff501b
kelvin <kelvinmarquesk@gmail.com>
2023-10-25 08:28:38 -0300

    Add class and migration Usuario

---
+++ src/main/java/br/edu/ifrn/vendasestoque/domain/usuario/Usuario.java
@@ -0,0 +1,27 @@
+package br.edu.ifrn.vendasestoque.domain.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "usuario")
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
