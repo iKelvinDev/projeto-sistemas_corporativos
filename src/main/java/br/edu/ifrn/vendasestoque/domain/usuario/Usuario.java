b4a30b0ef8e7fa3e125fecf6680c3eae927e189b
kelvin <kelvinmarquesk@gmail.com>
2023-12-06 08:59:32 -0300

    Add Permissoes na classe Usuario

--- src/main/java/br/edu/ifrn/vendasestoque/domain/usuario/Usuario.java
+++ src/main/java/br/edu/ifrn/vendasestoque/domain/usuario/Usuario.java
@@ -2,15 +2,22 @@ package br.edu.ifrn.vendasestoque.domain.usuario;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import br.edu.ifrn.vendasestoque.domain.permissao.Permissao;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -26,14 +33,24 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(of = "id")
 public class Usuario implements UserDetails{
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String login;
   private String senha;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "usuario_permissao", 
+  joinColumns = @JoinColumn(name = "usuario_id"), 
+  inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+  private Set<Permissao> permissoes;
+
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    //return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    return this.permissoes.stream().map(
+      permissao -> new SimpleGrantedAuthority(permissao.getNome())).collect(Collectors.toList());
   }
   @Override
   public String getPassword() {
